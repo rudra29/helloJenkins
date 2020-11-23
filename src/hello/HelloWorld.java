0590a4521296a98df6e66c56c79290fea7a8db7a
rudra29 <rudra29@gmail.com>
2020-11-23 17:46:59 +0530

    2

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	
 	public static void main(String args[])
 	{
-		System.out.println("Started edat "+ new SimpleDateFormat("hh:mm:ss a").format(new Date()));
+		System.out.println("GIT "+ new SimpleDateFormat("hh:mm:ss a").format(new Date()));
 		for(int i=1;i<=5;i++)
 		{
 			System.out.println(i);
